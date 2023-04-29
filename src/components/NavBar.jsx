0d0cbc8a157a293b88f1f26eb3e7458d36b47311
ChelseyReann <chelseyreann@gmail.com>
2023-04-29 14:24:28 -0400

    updates NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <div>
-        <h1>Banana Bunch!!!!</h1>
-    </div>
+    <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/rules">Rules</NavLink>
+        <NavLink to="/cardsets">Card Sets</NavLink>
+        <NavLink to="/deckbuilder">Deck Builder</NavLink>
+        <NavLink to="deck">Deck</NavLink>
+    </nav>
   )
 }
