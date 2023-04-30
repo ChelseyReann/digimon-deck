f6eac4c31c3ce2fc878429fdfe71931d77a432da
ChelseyReann <chelseyreann@gmail.com>
2023-04-30 14:04:36 -0400

    updates CSS for NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom'
 export default function NavBar() {
   return (
     <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/rules">Rules</NavLink>
-        <NavLink to="/cardsets">Card Sets</NavLink>
-        <NavLink to="/deckbuilder">Deck Builder</NavLink>
-        <NavLink to="deck">Deck</NavLink>
+        <NavLink to="/" activeClassName="active-link">Home</NavLink>
+        <NavLink to="/rules" activeClassName="active-link">Rules</NavLink>
+        <NavLink to="/cardsets" activeClassName="active-link">Card Sets</NavLink>
+        <NavLink to="/deckbuilder" activeClassName="active-link">Deck Builder</NavLink>
+        <NavLink to="deck" activeClassName="active-link">Deck</NavLink>
     </nav>
   )
 }
