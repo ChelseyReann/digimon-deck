3932b01acc889689bc817412d2abe41e885c600a
CrissCross56 <grant.terdoslavich@gmail.com>
2023-05-01 01:28:45 -0400

    started some work on the routes, but i need to add some elements to the routes in app.js, will do more when names of components to import are revisited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {Links, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
     <>
       <NavBar />
+
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/rules"></Route>
+        <Route path="/cardsets"></Route>
+        <Route path="/deckbuilder"></Route>
+        <Route path="deck"></Route>
+      </Routes>
     </>
   );
 }
