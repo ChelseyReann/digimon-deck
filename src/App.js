af3eab8e51da5e36c322c8641feac8eb9a0a2664
CrissCross56 <grant.terdoslavich@gmail.com>
2023-05-01 14:24:05 -0400

    added more routes for signing in, up, and out, also added useEffect and useState to app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import './App.css';
 
 import {Links, Routes, Route} from 'react-router-dom'
-
-
-import NavBar from './components/NavBar';
-
+import { useState, useEffect } from 'react';
+import Home from './screens/Home.jsx';
+import NavBar from './components/NavBar.jsx';
+import RulesPage from './screens/RulesPage.jsx';
+import CardSets from './screens/CardSetPage.jsx';
+import DeckBuilderPage from './screens/DeckBuilderPage.jsx';
+import SignUp from './screens/SignUpPage.jsx';
+import SignIn from './screens/SignInPage.jsx';
+import SignOut from './screens/SignOutPage.jsx';
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser()
+      user ? setUser(user) : setUser(null)
+    }
+    fetchUser()
+  }, [])
+
+
   return (
     <>
       <NavBar />
 
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/rules"></Route>
-        <Route path="/cardsets"></Route>
-        <Route path="/deckbuilder"></Route>
+        <Route path="/" element={<Home user={user}/>} />
+        <Route path="/rules" element={<RulesPage/>}></Route>
+        <Route path="/cardsets" element={<CardSets/>}></Route>
+        <Route path="/deckbuilder" element={<DeckBuilderPage/>}></Route>
         <Route path="deck"></Route>
+        <Route path="/sign-up" element={<SignUp setUser={setUser}/>}/>
+        <Route path="/sign-in" element={<SignIn setUser={setUser}/>}/>
+        <Route path="/sign-out" element={<SignOut setUser={setUser}/>} />
       </Routes>
     </>
   );
