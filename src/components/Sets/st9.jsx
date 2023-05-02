861a7c9ca5fb951f9b6b65972323a65f4b1939b6
ChelseyReann <chelseyreann@gmail.com>
2023-05-02 12:13:34 -0400

    adds card sets st9

---
+++ src/components/Sets/st9.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+export default function st9() {
+    const [cards, setCards] = useState ([])
+
+    useEffect(() => {
+        const cardSets = async () => {
+            await axios.get("https://digimon-api.herokuapp.com/setname/ST-9: Starter Deck Ultimate Ancient Dragon")
+            setCards(response)
+        }
+    }, [])
+
+
+  return (
+    <div>
+        {cards.map ((card, index) => (
+            <div key={index} style={{backgroundImage:`url(${card.image_url})`}}></div>
+        ))}
+    </div>
+  )
+}
