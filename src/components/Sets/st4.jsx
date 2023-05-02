48ec7fc2294cf35c9d5f29aeb1fc7bd699a2076e
ChelseyReann <chelseyreann@gmail.com>
2023-05-02 12:12:09 -0400

    adds card sets st6

--- src/components/Sets/st4.jsx
+++ src/components/Sets/st4.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
-export default function bt10() {
+export default function st4() {
     const [cards, setCards] = useState ([])
 
     useEffect(() => {
