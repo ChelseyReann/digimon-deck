ae1c95f01eaabdc857a0f0a0222771cf2e61200d
CrissCross56 <grant.terdoslavich@gmail.com>
2023-05-02 12:36:38 -0400

    the filtering logic for the search component, works,it will need to have the results clickable to return metadata about that result, so it can be displayed on the page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Search() {
+
+  let testArray = [
+    {
+      id: 1,
+      name: "Grant",
+      age: 25
+    },
+    {
+      id: 2,
+      name: "Nelson",
+      age: 30
+    },
+    {
+      id: 3,
+      name: "Chelsea",
+      age: 25
+    },
+    {
+      id: 4,
+      name: "Guy",
+      age: 22
+    }
+  ]
+
+  const [searchQuery, setSearchQuery] = useState('');
+                                                // replace with props
+  const [searchResults, setSearchResults] = useState(testArray);
+
+  function handleChange(e){
+    setSearchQuery(e.target.value);
+    setSearchResults(getFilteredResults(e.target.value))
+  }
+
+  function getFilteredResults(query){
+                // replace test array with props
+    let results = testArray.filter((item) => {
+      return item.name.toLowerCase().includes(query.toLowerCase());
+    })
+    console.log(`this is the results of filter ${results}`);
+    return results;
+  }
+
   return (
-    <div>Search</div>
+    <div>
+      <form>
+        <input type="text" value={searchQuery} placeholder='Search' onChange={handleChange}></input>
+      </form>
+      {
+        searchResults.map((result)=>(<div key={result.id}>{result.name}</div>))
+      }
+    </div>
   )
 }
