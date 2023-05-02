205b529b881a038dd4e8b06ecc1115f46f18e7b8
ChelseyReann <chelseyreann@gmail.com>
2023-05-02 12:07:25 -0400

    adds card sets components

---
+++ src/components/Sets/bt2.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+export default function bt2() {
+    const [cards, setCards] = useState ([])
+
+    useEffect(() => {
+        const cardSets = async () => {
+            await axios.get("https://digimon-api.herokuapp.com/setname/BT01-03: Release Special Booster Ver.1.5")
+            setCards(response)
+        }
+    }, [])
+
+
+  return (
+    <div>
+        {cards.map ((card, index) => (
+            <div key={index} style={{backgroundImage:`url(${card.image_url})`}}></div>
+        ))}
+    </div>
+  )
+}
---
+++ src/components/Sets/bt8.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+export default function bt8() {
+    const [cards, setCards] = useState ([])
+
+    useEffect(() => {
+        const cardSets = async () => {
+            await axios.get("https://digimon-api.herokuapp.com/setname/New Awakening Pre-Release Pack")
+            setCards(response)
+        }
+    }, [])
+
+
+  return (
+    <div>
+        {cards.map ((card, index) => (
+            <div key={index} style={{backgroundImage:`url(${card.image_url})`}}></div>
+        ))}
+    </div>
+  )
+}
