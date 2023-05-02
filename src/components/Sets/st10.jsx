fbfa4fac96e0624ead0284678b6f6d2c022c97cc
ChelseyReann <chelseyreann@gmail.com>
2023-05-02 12:14:17 -0400

    adds card sets st10

---
+++ src/components/Sets/st10.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+export default function st10() {
+    const [cards, setCards] = useState ([])
+
+    useEffect(() => {
+        const cardSets = async () => {
+            await axios.get("https://digimon-api.herokuapp.com/setname/ST-10: Starter Deck Parallel World Tactician")
+            setCards(response)
+        }
+    }, [])
+
+
+  return (
+    <div>
+        {cards.map ((card, index) => (
+            <div key={index} style={{backgroundImage:`url(${card.image_url})`}}></div>
+        ))}
+    </div>
+  )
+}
