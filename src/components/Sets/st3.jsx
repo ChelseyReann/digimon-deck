1e4b5bd04e55598bd42f759296951e87802cb69a
ChelseyReann <chelseyreann@gmail.com>
2023-05-02 12:09:03 -0400

    adds card sets st3

---
+++ src/components/Sets/st3.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+export default function bt10() {
+    const [cards, setCards] = useState ([])
+
+    useEffect(() => {
+        const cardSets = async () => {
+            await axios.get("https://digimon-api.herokuapp.com/setname/ST-3: Starter Deck Heaven's Yellow")
+            setCards(response)
+        }
+    }, [])
+
+
+  return (
+    <div>
+        {cards.map ((card, index) => (
+            <div key={index} style={{backgroundImage:`url(${card.image_url})`}}></div>
+        ))}
+    </div>
+  )
+}
