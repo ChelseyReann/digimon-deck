9605da083f4e9fcfbf76df926cce14fb42ce8751
ChelseyReann <chelseyreann@gmail.com>
2023-05-04 12:59:42 -0400

    updates NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,26 +6,17 @@ export default function NavBar({ user }) {
       <div>
         {user && <div className="link welcome">Welcome, {user.username}</div>}
       </div>
-      <NavLink to="/" activeclassname="active-link">
-        Home
-      </NavLink>
-      <NavLink to="/rules" activeclassname="active-link">
-        Rules
-      </NavLink>
-      <NavLink to="/cardsets" activeclassname="active-link">
-        Card Sets
-      </NavLink>
+      <NavLink to="/" activeclassname="active-link">Home</NavLink>
+      <NavLink to="/rules" activeclassname="active-link">Rules</NavLink>
+      <NavLink to="/cardsets" activeclassname="active-link">Card Sets</NavLink>
       {user ? (
         <NavLink to={`/deck1/${user.id}`} activeclassname="active-link">
            Deck
         </NavLink>
       ) : null}
-      <NavLink to="/sign-up" activeclassname="active-link">
-        Sign-up
-      </NavLink>
-      <NavLink to="/sign-in" activeclassname="active-link">
-        Sign-in
-      </NavLink>
+      
+      <NavLink to="/sign-up" activeclassname="active-link">Sign-up</NavLink>
+      <NavLink to="/sign-in" activeclassname="active-link">Sign-in</NavLink>
     </nav>
   );
 }
