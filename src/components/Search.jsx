b432e95c59efa1872121b6bf5f50f78bccc943f5
ChelseyReann <chelseyreann@gmail.com>
2023-05-04 20:24:28 -0400

    updates Search.jsx & Search.css

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 import "./Sets/Search/Search.css";
 
 export default function Search(props) {
@@ -164,11 +163,6 @@ export default function Search(props) {
       <div className="parent" onClick={openUiButton}>
         <div className="overflow">
         <div className='search-bar'>
-        
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M16.5 12L7.5 12" stroke="#000" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M13.5 9L16.5 12L13.5 15" stroke="#000" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
           <form className="grant-form">
             <input
               type="text"
@@ -216,36 +210,38 @@ export default function Search(props) {
           </div>
         </div>
 
+         <div className="currentCardInfo">
+          <p>Name: {currentName}</p>
+          <p>Color: {currentColor}</p>
+          <p>Type: {currentType}</p>
+          <p>Attribute: {currentAttribute}</p>
+          <p>Level: {currentLevel}</p>
+          <p>Play Cost: {currentPlayCost}</p>
+          <p>Evolution Cost: {currentEvolutionCost}</p>
+          <p>DP: {currentDp}</p>
+          <p>Card Number: {currentCardNumber}</p>
+          <p>Main Effect: {currentMainEffect}</p>
+          <p>Source Effect: {currentSourceEffect}</p>
+          <p>Set Name: {currentSetName}</p>
+        </div>
+
         <div
           key={currentCard._id}
           id="cardDisplay"
           style={{
             backgroundImage: `url(${currentCard})`,
-            width: "25rem",
-            height: "35rem",
+            width: "18rem",
+            height: "25rem",
             backgroundRepeat: "no-repeat",
             backgroundSize: "cover",
-            marginLeft: "150%",
-            marginTop: "30%",
+            // marginLeft: "35%",
+            marginTop: "6%",
             borderRadius: "1.5rem",
           }}
         >
          
         </div>
-        <div className="currentCardInfo">
-          {currentName}
-          {currentColor}
-          {currentType}
-          {currentAttribute}
-          {currentLevel}
-          {currentPlayCost}
-          {currentEvolutionCost}
-          {currentDp}
-          {currentCardNumber}
-          {currentMainEffect}
-          {currentSourceEffect}
-          {currentSetName}
-        </div>
+       
       </div>
     </div>
   );
